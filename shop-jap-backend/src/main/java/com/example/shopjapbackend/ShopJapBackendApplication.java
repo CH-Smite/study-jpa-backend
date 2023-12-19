2e30bde6b46a7ccb28cdadc042d25438f3611d00
CH-Smite <>
2023-12-20 00:38:52 +0900

    🎉 Add project

---
+++ shop-jap-backend/src/main/java/com/example/shopjapbackend/ShopJapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopjapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopJapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopJapBackendApplication.class, args);
+	}
+
+}
---
+++ shop-jap-backend/src/test/java/com/example/shopjapbackend/ShopJapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopjapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
