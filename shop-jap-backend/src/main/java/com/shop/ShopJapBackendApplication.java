f589c53350c8efa54865dbc9a7a62ea1b8fb69ce
good_morningross <ryu4714@gmail.com>
2024-07-31 12:52:48 +0900

    ✨feat: "Building a shopping mall"
    상품등록, 상품관리, 장바구니, 구매이력 구현

---
+++ shop-jap-backend/src/main/java/com/shop/ShopJapBackendApplication.java
@@ -0,0 +1,15 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ShopJapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopJapBackendApplication.class, args);
+    }
+
+}
